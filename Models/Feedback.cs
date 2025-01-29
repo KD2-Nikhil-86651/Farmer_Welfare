40b1c45dd3ce34fea2a28201f3d168c73675494d
Nihil <nikhilshinde2598@gmail.com>
2025-01-29 23:29:18 +0530

    Feedback and Shopkeeper model

---
+++ Models/Feedback.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmerBackend.Models;
+
+public partial class Feedback
+{
+    public int FeedbackId { get; set; }
+
+    public string Feedback1 { get; set; } = null!;
+}
---
+++ Models/Shopkeeper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmerBackend.Models;
+
+public partial class Shopkeeper
+{
+    public int ShopkeeperId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string ContactNo { get; set; } = null!;
+
+    public string LicenseNo { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int FarmerId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int Rate { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public int? BuyerId { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public DateTime? Timestamp { get; set; }
+
+    public virtual Buyer? Buyer { get; set; }
+
+    public virtual ICollection<Buyer> Buyers { get; set; } = new List<Buyer>();
+
+    public virtual Category Category { get; set; } = null!;
+
+    public virtual Farmer Farmer { get; set; } = null!;
+
+    public virtual ICollection<Farmer> Farmers { get; set; } = new List<Farmer>();
+}
