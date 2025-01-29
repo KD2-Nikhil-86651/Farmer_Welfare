a9cd4ca765e9fe63db7fb58276552fb3c5c5e8f1
ashish prasad <ashishprasad3270@gmail.com>
2025-01-29 23:33:21 +0530

    farmer and crop

---
+++ Models/Crop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmerBackend.Models;
+
+public partial class Crop
+{
+    public int CropId { get; set; }
+
+    public string CropName { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public int Rate { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+
+    public virtual ICollection<Farmer> Farmers { get; set; } = new List<Farmer>();
+}
---
+++ Models/Farmer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmerBackend.Models;
+
+public partial class Farmer
+{
+    public int FarmerId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string ContactNo { get; set; } = null!;
+
+    public DateOnly Dob { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public int? CropId { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public int? ShopkeeperId { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public DateTime? Timestamp { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual Crop? Crop { get; set; }
+
+    public virtual Shopkeeper? Shopkeeper { get; set; }
+
+    public virtual ICollection<Shopkeeper> Shopkeepers { get; set; } = new List<Shopkeeper>();
+}
