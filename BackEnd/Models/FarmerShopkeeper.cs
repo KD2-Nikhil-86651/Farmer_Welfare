0ab60e5735bc32c2dcf83ed3619e941d2485b1b2
KD2-86675-Yashwant <dope.yash1999@gmail.com>
2025-02-18 19:02:50 +0530

    new file added

---
+++ BackEnd/Models/FarmerShopkeeper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmerBackend.Models;
+
+public partial class FarmerShopkeeper
+{
+    public int FarmerShopkeeperId { get; set; }
+
+    public int FarmerId { get; set; }
+
+    public int ShopkeeperId { get; set; }
+
+    public DateTime? Timestamp { get; set; }
+
+    public virtual Farmer Farmer { get; set; } = null!;
+
+    public virtual Shopkeeper Shopkeeper { get; set; } = null!;
+}
