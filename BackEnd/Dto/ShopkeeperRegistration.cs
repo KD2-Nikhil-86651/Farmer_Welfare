520e3b59f846a580b49430ce2f850e25b7e8583a
KD2-86675-Yashwant <dope.yash1999@gmail.com>
2025-02-18 19:06:34 +0530

    new file added

--- BackEnd/Dto/ShopkeeperRegistration.cs
+++ BackEnd/Dto/ShopkeeperRegistration.cs
@@ -2,9 +2,9 @@
 {
     public class ShopkeeperRegistration
     {
-        public string Fname { get; set; }
-        public string Lname { get; set; }
-        public string Contact { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
         public string LicenseNo { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
