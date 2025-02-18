97ec9ebe029425b5876018baf9ac1e236184ca85
KD2-86675-Yashwant <dope.yash1999@gmail.com>
2025-02-18 19:05:06 +0530

    new file added

---
+++ BackEnd/Dto/UpdateCropRateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarmerBackend.Dto
+{
+    public class UpdateCropRateRequest
+    {
+
+        //public int CropId { get; set; }
+        //public int NewRate { get; set; }
+        //public string AdminUser { get; set; }
+        public int CropId { get; set; }
+
+        [Required(ErrorMessage = "New rate is required.")]
+        public int NewRate { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required.")]
+        public string ContactNo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Role is required.")]
+        public string Role { get; set; } = string.Empty;
+    }
+}
