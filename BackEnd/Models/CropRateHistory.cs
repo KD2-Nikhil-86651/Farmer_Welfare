cf54c6a20d4f40d2323bc54c4f63f2610ecbdaa5
KD2-86675-Yashwant <dope.yash1999@gmail.com>
2025-02-18 19:01:33 +0530

    new file added

---
+++ BackEnd/Models/CropRateHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmerBackend.Models;
+
+public partial class CropRateHistory
+{
+    public int HistoryId { get; set; }
+
+    public int CropId { get; set; }
+
+    public int Rate { get; set; }
+
+    public DateTime? RateDate { get; set; } // ✅ Changed from DateOnly? to DateTime?
+
+    public string UpdatedBy { get; set; } = null!;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Crop Crop { get; set; } = null!;
+}
